e36bee979619827e3f6e97304f7a330268bcfea6
Dan Nafe <103373716+stayAtHomeAstronaut@users.noreply.github.com>
2025-04-23 17:38:38 -0400

    Update my_lyc.py

--- my_lyc.py
+++ my_lyc.py
@@ -19,13 +19,16 @@ warnings.simplefilter('ignore', category=AstropyWarning)
 
 # === User Inputs ===
 fits_folder = '/Volumes/SSDonUSB/astro_pics/TIC_67646988/Light4Split_d/'  # folder containing FITS files
-#"TIC 67646988"
+target_name = "TIC 67646988"
 target_ra = 147.769065  # example RA in degrees
 target_dec = 35.969295  # example DEC in degrees
 
-#XO-6
-target_ra = 94.793212  # example RA in degrees
-target_dec = 73.827663  # example DEC in degrees
+
+#fits_folder = '/Volumes/SSDonUSB/astro_pics/XO-6/demosaiced/'
+fits_folder = '/Volumes/SSDonUSB/astro_pics/XO-6/Light_d_a/'
+target_name = 'XO-6'
+target_ra = 94.793212  # RA in degrees
+target_dec = 73.827663  # DEC in degrees
 aperture_radius = 5.0  # in pixels
 
 # === Convert RA/DEC to SkyCoord ===
@@ -34,22 +37,21 @@ target_coord = SkyCoord(ra=target_ra*u.deg, dec=target_dec*u.deg)
 times = []
 times_red = []
 times_blue = []
+times_green = []
 fluxes = []
 fluxes_red = []
 fluxes_blue = []
+fluxes_green =[]
 ratio_array = []
 
 colors = []
 colors.append('red')
 colors.append('blue')
+colors.append('green')
 for color in colors:
-    fits_folder = '/Volumes/SSDonUSB/astro_pics/XO-6/Light_d_a/' + color
-    fits_folder = '/Volumes/SSDonUSB/astro_pics/TIC_67646988/Light4Split_d/' + color
-    fits_folder = '/Volumes/SSDonUSB/astro_pics/XO-6/demosaiced/' + color
     print (fits_folder)
     # === Collect FITS Files ===
-    fits_files = sorted(glob.glob(os.path.join(fits_folder, '*.fits')))
-    
+    fits_files = sorted(glob.glob(os.path.join(fits_folder,color, '*.fits')))    
     
     # === Loop Through Each FITS Image ===
     for file in fits_files:
@@ -87,6 +89,9 @@ for color in colors:
                 if color == 'blue':
                 	times_blue.append(time)
                 	fluxes_blue.append(flux)
+                if color == 'green':
+                	times_green.append(time)
+                	fluxes_green.append(flux)
     
 # === Sort by time and convert to numpy arrays ===
 times = np.array(times)
@@ -107,52 +112,51 @@ sorted_indices = np.argsort(times_blue)
 times_blue = times_blue[sorted_indices]
 fluxes_blue = fluxes_blue[sorted_indices]
 
-rows = len(times_red)
-for i in range(rows):
-	if (times_red[i] == times_blue[i]):
-		ratio_array.append(10000*(fluxes_blue[i]/fluxes_red[i]))
-		
-#print(times_red == times_blue)
-obs_time_list = []
+times_green = np.array(times_green)
+fluxes_green = np.array(fluxes_green)
+sorted_indices = np.argsort(times_green)
+times_green = times_green[sorted_indices]
+fluxes_green = fluxes_green[sorted_indices]
+
 
 rows = len(times_red)
+obs_time_list = []
 for i in range(rows):
     obs_time = Time(times_red[i], format='jd')
     obs_time_list.append(obs_time.iso)
+    if (times_red[i] == times_blue[i]):
+        ratio_array.append(10000*(fluxes_blue[i]/fluxes_red[i]))
 	
-cols = 5
+cols = 6
 #always append to a list, then concatinate into a dataframe
 row_list = []
 for i in range(rows):
-    row_list.append({"obs_date":obs_time_list[i],"JD":times_red[i],"flux_red":fluxes_red[i],"flux_blue":fluxes_blue[i],"blue_over_red":fluxes_blue[i]/fluxes_red[i]})
+    row_list.append({"obs_date":obs_time_list[i],"JD":times_red[i],"flux_red":fluxes_red[i],"flux_green":fluxes_green[i],"flux_blue":fluxes_blue[i],"blue_over_red":fluxes_blue[i]/fluxes_red[i]})
 
 df = pd.concat([pd.DataFrame([row]) for row in row_list], ignore_index=True)
 
-#appending to a dataframe is not good for large datasets
-#df = pd.DataFrame({'obs_date': pd.Series(dtype='str'),'JD': pd.Series(dtype='float'),'flux_red': pd.Series(dtype='float'),'flux_blue': pd.Series(dtype='float'),'blue_over_red': pd.Series(dtype='float')})
-#for i in range(rows):
-    #df.loc[len(df)] = [obs_time_list[i],times_red[i],fluxes_red[i],fluxes_blue[i],(fluxes_blue[i]/fluxes_red[i])]
-
 print(df)
-df.to_csv('output.csv', index=False)
-df[np.abs(stats.zscore(df['blue_over_red'])) < 2].to_csv('reject_outliers_output.csv',index=False)
-#df2 =  df[np.abs(stats.zscore(df['blue_over_red'])) < 2]
-#df2.rolling(3).mean().to_csv('rolling_reject_output.csv',on='blue_over_red',index=False)
+outfile = target_name + '_rgb_output.csv'
+df.to_csv(outfile, index=False)
+
+
 q_low = df["blue_over_red"].quantile(0.01)
 q_hi  = df["blue_over_red"].quantile(0.99)
 df_filtered = df[(df["blue_over_red"] < q_hi) & (df["blue_over_red"] > q_low)]
-df_filtered.to_csv('quantile_filtered_output.csv', index=False)
+outfile = outfile = target_name + '_quantile_filtered_output.csv'
+df_filtered.to_csv(outfile, index=False)
 
 # === Plot Light Curve ===
 plt.figure(figsize=(10, 5))
 plt.plot(times, fluxes, 'o-', color='black')
 plt.plot(times_red, fluxes_red, 'o-', color='red')
 plt.plot(times_blue, fluxes_blue, 'o-', color='blue')
+plt.plot(times_green, fluxes_green, 'o-', color='green')
 plt.plot(times_red, ratio_array, 'o-', color='grey')
 
 plt.xlabel('Julian Date')
 plt.ylabel('Flux (ADU)')
-plt.title('Light Curve')
+plt.title(target_name)
 plt.grid(True)
 plt.tight_layout()
 plt.show()
